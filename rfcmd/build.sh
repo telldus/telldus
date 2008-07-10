935e97c77d55b603bc728a5144d18a2f88feb67b
Micke Prag <micke.prag@telldus.se>
2008-07-10 08:32:26 +0000

    Added 'make install' to rfcmd makefile. Thanks to Hans Winzell

--- rfcmd/build.sh
+++ rfcmd/build.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-make clean
-make
-echo rfcmd built!
-
+set -e
+make clean || { echo "Warning: make clean failed"; }
+make || { echo "make failed"; exit 1; }
+make install || { echo "make install failed"; exit 1; }
+echo rfcmd built and installed!
