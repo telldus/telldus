1b763a65b6048cb38f8beee58ea384226f275f71
Micke Prag <micke.prag@telldus.se>
2009-01-07 11:25:51 +0000

    Switched to std::string in Settings

--- driver/libtelldus-core/Settings.h
+++ driver/libtelldus-core/Settings.h
@@ -9,16 +9,16 @@ class Settings
 {
 public:
 	Settings(void);
-	const char * getSetting(const char *strName);
+	std::string getSetting(const std::string &strName);
 	int getNumberOfDevices(void);
-	char* getName(int intDeviceId);
-	bool setName(int intDeviceId, const char* strNewName);
-	char* getProtocol(int intDeviceId);
-	bool setProtocol(int intDeviceId, const char* strVendor);
+	std::string getName(int intDeviceId);
+	bool setName(int intDeviceId, const std::string &strNewName);
+	std::string getProtocol(int intDeviceId);
+	bool setProtocol(int intDeviceId, const std::string &strVendor);
 	int getModel(int intDeviceId);
 	bool setModel(int intDeviceId, int intModel);
-	char* getDeviceParameter(int intDeviceId, const char *strName);
-	bool setDeviceParameter(int intDeviceId, const char *strName, const char *strValue);
+	std::string getDeviceParameter(int intDeviceId, const std::string &strName);
+	bool setDeviceParameter(int intDeviceId, const std::string &strName, const std::string &strValue);
 	int addDevice();
 	int getDeviceId(int intDeviceIndex);
 	bool removeDevice(int intDeviceId);
@@ -26,10 +26,10 @@ public:
 	~Settings(void);
 	
 protected:
-	char *getStringSetting(int intDeviceId, const char* name, bool parameter);
-	bool setStringSetting(int intDeviceId, const char* name, const char *value, bool parameter);
-	int getIntSetting(int intDeviceId, const char* name, bool parameter);
-	bool setIntSetting(int intDeviceId, const char* name, int value, bool parameter);
+	std::string getStringSetting(int intDeviceId, const std::string &name, bool parameter);
+	bool setStringSetting(int intDeviceId, const std::string &name, const std::string &value, bool parameter);
+	int getIntSetting(int intDeviceId, const std::string &name, bool parameter);
+	bool setIntSetting(int intDeviceId, const std::string &name, int value, bool parameter);
 
 private:
 	int getNextDeviceId();
