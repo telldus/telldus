80a30d6a1cbbf9da1f618ec96a80584ec43406f0
Micke Prag <micke.prag@telldus.se>
2009-03-25 20:11:57 +0000

    Added support for RisingSun receivers

---
+++ driver/libtelldus-core/DeviceRisingSun.h
@@ -0,0 +1,31 @@
+#pragma once
+#include "Device.h"
+#include <string>
+
+namespace TelldusCore {
+	
+	class DeviceRisingSun : public Device
+	{
+	public:
+		DeviceRisingSun(int id, int model, const std::string &name);
+		virtual int methods(int methodsSupported);
+		virtual std::string getProtocol() const;
+		virtual bool parameterMatches( const std::string &name, const std::string &value ) const;
+		
+		bool setHouse(const std::string &newHouse);
+		bool setUnit(const std::string &newUnit);
+		
+	public:
+		~DeviceRisingSun(void);
+	
+	protected:
+		virtual bool setDeviceParameter(const std::string &strName, const std::string &strValue);
+		virtual int turnOn(void);
+		virtual int turnOff(void);
+
+		int intHouse;
+		int intCode;
+		std::string getStringCode(int);
+	};
+	
+}
