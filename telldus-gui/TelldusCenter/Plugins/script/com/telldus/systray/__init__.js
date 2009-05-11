b53c96e06ff0532937cbb3a0217c47979fb8ecd4
Micke Prag <micke.prag@telldus.se>
2009-05-11 12:29:09 +0000

    Open the mainwindow when the systray-icon is clicked.

--- telldus-gui/TelldusCenter/Plugins/script/com/telldus/systray/__init__.js
+++ telldus-gui/TelldusCenter/Plugins/script/com/telldus/systray/__init__.js
@@ -1,10 +1,15 @@
 
 __postInit__ = function() {
 	application.showMessage.connect( systrayMessage );
+	com.telldus.systray.triggered.connect( triggered );
 }
 
 function systrayMessage(title, message, detailedMessage) {
 	if (!application.isMainWindowShown()) {
 		com.telldus.systray.showMessage(title, message, detailedMessage);
 	}
+}
+
+function triggered() {
+	application.showMainWindow();
 }
