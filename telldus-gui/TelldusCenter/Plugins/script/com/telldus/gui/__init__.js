598af8df363c6b909a9c7d0dd6ed1dcf2cb00eca
Micke Prag <micke.prag@telldus.se>
2009-08-13 18:14:01 +0000

    Added scriptfile to plugin com.telldus.gui

---
+++ telldus-gui/TelldusCenter/Plugins/script/com/telldus/gui/__init__.js
@@ -0,0 +1,44 @@
+/** TELLDUS GUI **/
+
+var devices = new Array();
+
+__postInit__ = function() {
+	com.telldus.gui.deviceChange.connect(com.telldus.core.deviceChange);
+	com.telldus.core.deviceChange.connect(deviceChange);
+	application.allDoneLoading.connect( allDone );
+}
+
+function allDone() {
+	if (com.telldus.systray) {
+		var count = com.telldus.core.getNumberOfDevices();
+		for (var i = 0; i < count; ++i) {
+			var id = com.telldus.core.getDeviceId(i);
+			var name = com.telldus.core.getName(id);
+			var menuId = com.telldus.systray.addMenuItem( name );
+
+			devices.push({
+				'id': id,
+				'menuId': menuId
+			});
+		}
+
+		com.telldus.systray.addSeparator();
+	}
+}
+
+function deviceChange( deviceId, eventType ) {
+	if (eventType != 2) {
+		return;
+	}
+	for (var i=0; i < devices.length; ++i) {
+		if (devices[i].id == deviceId) {
+			var name = com.telldus.core.getName(deviceId);
+			var menuItem = com.telldus.systray.menuItem( devices[i].menuId );
+			if (menuItem) {
+				menuItem.text = name;
+			}
+			break;
+		}
+	}
+}
+
