bbc5903b1ca29b4883c003fe2c82a83797016153
Micke Prag <micke.prag@telldus.se>
2009-08-14 11:47:11 +0000

    Added telldus-gui constants to script engine

--- telldus-gui/Plugins/Devices/devicesplugin.cpp
+++ telldus-gui/Plugins/Devices/devicesplugin.cpp
@@ -24,6 +24,10 @@ void DevicesPlugin::initialize ( const QString & key, QScriptEngine * engine ) {
 		connect(deviceWidget, SIGNAL(eventTriggered(const QString &, const QString &)), qApp, SLOT(eventTriggered(const QString &, const QString &)));
 
 		QScriptValue value = engine->newQObject(deviceWidget);
+		value.setProperty("TELLSTICK_DEVICE_ADDED", TELLSTICK_DEVICE_ADDED);
+		value.setProperty("TELLSTICK_DEVICE_CHANGED", TELLSTICK_DEVICE_CHANGED);
+		value.setProperty("TELLSTICK_DEVICE_REMOVED", TELLSTICK_DEVICE_REMOVED);
+		value.setProperty("TELLSTICK_DEVICE_STATE_CHANGED", TELLSTICK_DEVICE_STATE_CHANGED);
 		engine->globalObject().property("com").property("telldus").setProperty("gui", value);
 	}
 }
