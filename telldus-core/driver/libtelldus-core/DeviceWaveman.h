7a3618a18d4a37251afe67abe1d1618f4610cf56
Micke Prag <micke.prag@telldus.se>
2009-09-03 08:26:39 +0000

    Fixed bug where all Waveman devices was shown as Arctech, closes #50

--- telldus-core/driver/libtelldus-core/DeviceWaveman.h
+++ telldus-core/driver/libtelldus-core/DeviceWaveman.h
@@ -9,6 +9,7 @@ namespace TelldusCore {
 	public:
 		DeviceWaveman(int id, const std::string &model, const std::string &name);
 		virtual int methods();
+		virtual std::string getProtocol() const;
 		
 	protected:
 		virtual int turnOff(void);
