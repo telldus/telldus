4ea7172c8570ec59ac55becb17c0824683ecd006
Micke Prag <micke.prag@telldus.se>
2009-09-09 15:15:33 +0000

    Added generated TS-files

---
+++ telldus-gui/TelldusGui/telldus-gui_sv.ts
@@ -0,0 +1,227 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0">
+<context>
+    <name>DeviceModel</name>
+    <message>
+        <location filename="devicemodel.cpp" line="43"/>
+        <source>on</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="devicemodel.cpp" line="45"/>
+        <source>off</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="devicemodel.cpp" line="48"/>
+        <source>%1%</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="devicemodel.cpp" line="50"/>
+        <source>unknown %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="devicemodel.cpp" line="92"/>
+        <source>State</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="devicemodel.cpp" line="94"/>
+        <source>Device name</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="devicemodel.cpp" line="96"/>
+        <source>Action</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>DeviceSettingArctechSelflearning</name>
+    <message>
+        <location filename="devicesettingarctechselflearning.cpp" line="31"/>
+        <source>Remote code</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="devicesettingarctechselflearning.cpp" line="37"/>
+        <source>Unitcode</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>DeviceSettingIkea</name>
+    <message>
+        <location filename="devicesettingikea.cpp" line="25"/>
+        <source>System</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="devicesettingikea.cpp" line="35"/>
+        <source>Units</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="devicesettingikea.cpp" line="53"/>
+        <source>Fade</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="devicesettingikea.cpp" line="58"/>
+        <source>Smooth</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="devicesettingikea.cpp" line="60"/>
+        <source>Instant</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>DeviceSettingNexa</name>
+    <message>
+        <location filename="devicesettingnexa.cpp" line="33"/>
+        <source>Housecode</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="devicesettingnexa.cpp" line="39"/>
+        <source>Unitcode</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>DeviceSettingNexaBell</name>
+    <message>
+        <location filename="devicesettingnexabell.cpp" line="29"/>
+        <source>Housecode</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>DeviceSettingUpm</name>
+    <message>
+        <location filename="devicesettingupm.cpp" line="27"/>
+        <source>House</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="devicesettingupm.cpp" line="35"/>
+        <source>Button</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="devicesettingupm.cpp" line="42"/>
+        <source>Button %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>DeviceWidget</name>
+    <message>
+        <location filename="devicewidget.cpp" line="39"/>
+        <source>New device...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="devicewidget.cpp" line="41"/>
+        <source>New group...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="devicewidget.cpp" line="45"/>
+        <source>New</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="devicewidget.cpp" line="55"/>
+        <source>Edit</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="devicewidget.cpp" line="63"/>
+        <source>Remove</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="devicewidget.cpp" line="116"/>
+        <source>Are you sure you want to remove the selected device?</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="devicewidget.cpp" line="117"/>
+        <source>The device will be removed permanently from this application as well as all other applications.</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>EditDeviceDialog</name>
+    <message>
+        <location filename="editdevicedialog.cpp" line="61"/>
+        <source>Device</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="editdevicedialog.cpp" line="74"/>
+        <source>&amp;Name:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="editdevicedialog.cpp" line="82"/>
+        <source>Choose a device above</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="editdevicedialog.cpp" line="88"/>
+        <source>Addresscode</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="editdevicedialog.cpp" line="147"/>
+        <source>You must choose a device</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="editdevicedialog.cpp" line="148"/>
+        <source>Please select the device you have.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="editdevicedialog.cpp" line="164"/>
+        <source>The device must have a name.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="editdevicedialog.cpp" line="165"/>
+        <source>Please fill in a name in the field under &apos;Name&apos;</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>MethodWidget</name>
+    <message>
+        <location filename="methodwidget.cpp" line="34"/>
+        <source>Learn</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="vendordevicetreeitem.cpp" line="123"/>
+        <source>Not a Telldus device-file</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>VendorDeviceModel</name>
+    <message>
+        <location filename="vendordevicemodel.cpp" line="52"/>
+        <source>Name</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
