bfee209b7a3e06bc98d0bf3200ef4600dbd03b9a
Micke Prag <micke.prag@telldus.se>
2009-09-22 16:46:51 +0000

    Loaded translation files correctly on MacOSX

--- telldus-gui/TelldusGui/telldusgui.cpp
+++ telldus-gui/TelldusGui/telldusgui.cpp
@@ -1,13 +1,23 @@
 #include "telldusgui.h"
 #include <QTranslator>
 #include <QCoreApplication>
+#include <QDir>
 #include <QDebug>
 
 #include "devicewidget.h"
 
 QWidget* WINAPI tdDeviceWidget( QWidget *parent ) {
 	QTranslator *translator = new QTranslator(parent);
+#ifdef Q_OS_MAC
+	QDir resDir = QDir(qApp->applicationDirPath());
+        if (resDir.dirName() == "MacOS") {
+                resDir.cdUp();
+                resDir.cd("resources");
+        }
+        translator->load("TelldusGui_" + QLocale::system().name(), resDir.absolutePath());
+#else
 	translator->load("TelldusGui_" + QLocale::system().name());
+#endif
 	qApp->installTranslator(translator);
 	return new DeviceWidget(parent);
 }
