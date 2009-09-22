c1f508b085ad0a747c9c5cfd59394b1e9b4467dc
Micke Prag <micke.prag@telldus.se>
2009-09-22 11:58:44 +0000

    Loaded translations in telldus-gui

--- telldus-gui/TelldusGui/telldusgui.cpp
+++ telldus-gui/TelldusGui/telldusgui.cpp
@@ -1,7 +1,13 @@
 #include "telldusgui.h"
+#include <QTranslator>
+#include <QCoreApplication>
+#include <QDebug>
 
 #include "devicewidget.h"
 
 QWidget* WINAPI tdDeviceWidget( QWidget *parent ) {
+	QTranslator *translator = new QTranslator(parent);
+	translator->load("TelldusGui_" + QLocale::system().name());
+	qApp->installTranslator(translator);
 	return new DeviceWidget(parent);
 }
