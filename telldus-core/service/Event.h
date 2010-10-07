fc7a28e5095c8b93d1a3cd390fe22cfef73ab3b4
Micke Prag <micke.prag@telldus.se>
2010-10-07 18:27:33 +0000

    Added type EVENT for unix

--- telldus-core/service/Event.h
+++ telldus-core/service/Event.h
@@ -5,8 +5,8 @@
 	#include <windows.h>
 	typedef HANDLE EVENT;
 #else
-	//#include <pthread.h>
-	//typedef pthread_mutex_t MUTEX;
+	#include <pthread.h>
+	typedef pthread_cond_t EVENT;
 #endif
 
 #endif //EVENT_H
