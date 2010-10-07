047f162947ca4b0c8c3140fed43ead744a7b3320
Micke Prag <micke.prag@telldus.se>
2010-10-07 18:31:15 +0000

    Added the new type SOCKET

--- telldus-core/common/Socket.h
+++ telldus-core/common/Socket.h
@@ -3,10 +3,17 @@
 
 #include <string>
 
+#ifdef _WINDOWS
+	#include <windows.h>
+	typedef HANDLE SOCKET;
+#else
+	typedef unsigned int SOCKET;
+#endif
+
 class Socket
 {
 public:
-	Socket(HANDLE hPipe);
+	Socket(SOCKET hPipe);
 	virtual ~Socket(void);
 
 	std::wstring read();
