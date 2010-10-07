34bb8fc81216b7a4a3cb8e6c4bfb7c3113734b2e
Micke Prag <micke.prag@telldus.se>
2010-10-07 18:26:58 +0000

    Removed Qt from main_unix.cpp

--- telldus-core/service/main_unix.cpp
+++ telldus-core/service/main_unix.cpp
@@ -1,22 +1,11 @@
 
-#include <QtCore/QCoreApplication>
-#include <QDebug>
-
-#include "TelldusCore.h"
 #include <signal.h>
 
 void shutdownHandler(int onSignal) {
-	qApp->quit();
 }
 
 int main(int argc, char **argv) {
-	QCoreApplication app(argc, argv);
-
 	/* Install signal traps for proper shutdown */
 	signal(SIGTERM, shutdownHandler);
-	signal(SIGINT, shutdownHandler);
-	
-	TelldusCore telldusCore;
-	
-    return app.exec();
+	signal(SIGINT, shutdownHandler);	
 }
