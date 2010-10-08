747c3a21158aa8cb5808f53b3faf24f6e4c193cd
Micke Prag <micke.prag@telldus.se>
2010-10-08 09:29:15 +0000

    Store the eventObjects and mark an event as signaled when needed.

--- telldus-core/service/EventHandler_win.cpp
+++ telldus-core/service/EventHandler_win.cpp
@@ -7,7 +7,7 @@
 class EventHandler::PrivateData {
 public:
 	HANDLE *eventArray;
-	std::list<Event *> eventList;
+	Event **eventObjectArray;
 	int eventCount;
 };
 
@@ -15,28 +15,36 @@ EventHandler::EventHandler() {
 	d = new PrivateData;
 	d->eventCount = 0;
 	d->eventArray = new HANDLE[0];
+	d->eventObjectArray = new Event*[0];
 }
 
 EventHandler::~EventHandler(void) {
+	delete[] d->eventObjectArray;
 	delete[] d->eventArray;
 	delete d;
 }
 
 Event *EventHandler::addEvent() {
 	Event *event = new Event(this);
+
 	HANDLE *newArray = new HANDLE[d->eventCount+1];
+	Event **newObjectArray = new Event*[d->eventCount+1];
 	for (int i = 0; i < d->eventCount; ++i) {
 		newArray[i] = d->eventArray[i];
+		newObjectArray[i] = d->eventObjectArray[i];
 	}
 	delete[] d->eventArray;
+	delete[] d->eventObjectArray;
 	d->eventArray = newArray;
+	d->eventObjectArray = newObjectArray;
 	d->eventArray[d->eventCount] = event->retrieveNative();
-	d->eventList.push_back(event);
+	d->eventObjectArray[d->eventCount] = event;
 	++d->eventCount;
 	return event;
 }
 
 void EventHandler::signal(Event *event) {
+	event->signal();
 }
 
 bool EventHandler::waitForAny() {
@@ -48,7 +56,7 @@ bool EventHandler::waitForAny() {
 	if (eventIndex >= d->eventCount) {
 		return false;
 	}
-
+	d->eventObjectArray[eventIndex]->setSignaled();
 	return true;
 
 }
