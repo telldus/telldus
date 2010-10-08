476251894eaac9d5633a47315421a3ee048524e3
Micke Prag <micke.prag@telldus.se>
2010-10-08 06:31:27 +0000

    Added function-stub for ConnectionListener in Linux

---
+++ telldus-core/service/ConnectionListener_unix.cpp
@@ -0,0 +1,23 @@
+#include "ConnectionListener.h"
+
+
+class ConnectionListener::PrivateData {
+public:
+};
+
+ConnectionListener::ConnectionListener(const std::wstring &name)
+{
+	d = new PrivateData;
+}
+
+ConnectionListener::~ConnectionListener(void) {
+	delete d;
+}
+
+void ConnectionListener::listen(Event *waitEvent){
+	
+}
+
+Socket *ConnectionListener::retrieveClientSocket(){
+	
+}
