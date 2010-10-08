1e43b60572ca23bc9c2cdfbe4c186935feb67cdd
Micke Prag <micke.prag@telldus.se>
2010-10-08 13:07:38 +0000

    Added our TelldusMain class in the main-function

--- telldus-core/service/main_unix.cpp
+++ telldus-core/service/main_unix.cpp
@@ -1,11 +1,19 @@
-
+#include "TelldusMain.h"
 #include <signal.h>
+#include <stdio.h>
+
+TelldusMain tm;
 
 void shutdownHandler(int onSignal) {
+	printf("Shutting down\n");
+	tm.stop();
 }
 
 int main(int argc, char **argv) {
 	/* Install signal traps for proper shutdown */
 	signal(SIGTERM, shutdownHandler);
-	signal(SIGINT, shutdownHandler);	
+	signal(SIGINT, shutdownHandler);
+
+	tm.start();
+	return 0;
 }
