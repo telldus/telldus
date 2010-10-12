3496a7eaa58fb9d8dc5cd9ddbc0727611eed5ba0
Stefan Persson <stefan.persson@telldus.se>
2010-10-12 09:01:21 +0000

    Class added

---
+++ telldus-core/service/DeviceManager.h
@@ -0,0 +1,15 @@
+#ifndef DEVICEMANAGER_H
+#define DEVICEMANAGER_H
+
+class DeviceManager
+{
+public:
+	DeviceManager();
+	~DeviceManager(void);
+
+private:
+	class PrivateData;
+	PrivateData *d;
+};
+
+#endif //DEVICEMANAGER_H
