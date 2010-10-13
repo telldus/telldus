9f0e3adbdca1d04a8cc2234315bdcf3e2ab582f8
Micke Prag <micke.prag@telldus.se>
2010-10-13 12:55:19 +0000

    Implemented TellStick::findAllByVIDPID() on Windows

--- telldus-core/service/TellStick_ftd2xx.cpp
+++ telldus-core/service/TellStick_ftd2xx.cpp
@@ -63,6 +63,46 @@ std::list<TellStickDescriptor> TellStick::findAll() {
 std::list<TellStickDescriptor> TellStick::findAllByVIDPID( int vid, int pid ) {
 	std::list<TellStickDescriptor> retval;
 	
+	FT_HANDLE fthHandle = 0;
+	FT_STATUS ftStatus = FT_OK;
+	DWORD dwNumberOfDevices = 0;
 
+#ifndef _WINDOWS
+	FT_SetVIDPID(vid, pid);
+#endif
+
+	ftStatus = FT_CreateDeviceInfoList(&dwNumberOfDevices);
+	if (ftStatus != FT_OK) {
+		return retval;
+	}
+	for (int i = 0; i < (int)dwNumberOfDevices; i++) { 
+		FT_PROGRAM_DATA pData;
+		char ManufacturerBuf[32]; 
+		char ManufacturerIdBuf[16]; 
+		char DescriptionBuf[64]; 
+		char SerialNumberBuf[16]; 
+
+		pData.Signature1 = 0x00000000;
+		pData.Signature2 = 0xffffffff;
+		pData.Version = 0x00000002;      // EEPROM structure with FT232R extensions
+		pData.Manufacturer = ManufacturerBuf; 
+		pData.ManufacturerId = ManufacturerIdBuf;
+		pData.Description = DescriptionBuf; 
+		pData.SerialNumber = SerialNumberBuf; 
+
+		ftStatus = FT_Open(i, &fthHandle);
+		ftStatus = FT_EE_Read(fthHandle, &pData);
+		ftStatus = FT_Close(fthHandle);
+		if (ftStatus != FT_OK) {
+			continue;
+		}
+		if (pData.VendorId == vid && pData.ProductId == pid) {
+			TellStickDescriptor td;
+			td.vid = vid;
+			td.pid = pid;
+			td.serial = pData.SerialNumber;
+			retval.push_back(td);
+		}
+	}
 	return retval;
 }
