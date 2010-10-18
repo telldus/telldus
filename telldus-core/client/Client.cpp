9d515851d2703cf2d6b135177d3707d46dcb22ae
Micke Prag <micke.prag@telldus.se>
2010-10-18 09:50:19 +0000

    Client now tries to connect an event socket

--- telldus-core/client/Client.cpp
+++ telldus-core/client/Client.cpp
@@ -1,11 +1,18 @@
 #include "Client.h"
+#include "Socket.h"
 
 using namespace TelldusCore;
 
+class Client::PrivateData {
+public:
+	Socket eventSocket;
+};
 
-Client::Client()
-{
+Client::Client() {
+	d = new PrivateData;
+	d->eventSocket.connect(L"TelldusEvents");
 }
 
 Client::~Client(void) {
+	delete d;
 }
