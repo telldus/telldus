4029342094122ad47c588f18c2c296ad1de0b4e2
Micke Prag <micke.prag@telldus.se>
2010-10-20 15:17:33 +0000

    Implemented TellStick_libftdi on Linux, stille some work is needed

--- telldus-core/service/TellStick_libftdi.cpp
+++ telldus-core/service/TellStick_libftdi.cpp
@@ -1,7 +1,7 @@
 //
 // C++ Implementation: TellStick
 //
-// Description: 
+// Description:
 //
 //
 // Author: Micke Prag <micke.prag@telldus.se>, (C) 2009
@@ -20,15 +20,23 @@
 class TellStick::PrivateData {
 public:
 	bool open;
+	int vid, pid, fwVersion;
+	std::string serial, message;
 	ftdi_context ftHandle;
 };
 
-TellStick::TellStick( const TellStickDescriptor &td ) {
+TellStick::TellStick(Event *event, const TellStickDescriptor &td )
+	:Controller(event)
+{
 	d = new PrivateData;
 	d->open = false;
+	d->vid = td.vid;
+	d->pid = td.pid;
+	d->fwVersion = 0;
+	d->serial = td.serial;
 
 	ftdi_init(&d->ftHandle);
-	
+
 	int ret = ftdi_usb_open_desc(&d->ftHandle, td.vid, td.pid, NULL, td.serial.c_str());
 	if (ret < 0) {
 		ftdi_deinit(&d->ftHandle);
@@ -37,7 +45,7 @@ TellStick::TellStick( const TellStickDescriptor &td ) {
 	d->open = true;
 	ftdi_usb_reset( &d->ftHandle );
 	ftdi_disable_bitbang( &d->ftHandle );
-	
+
 	if (d->open) {
 // 		setBaud(4800);
 	}
@@ -59,6 +67,23 @@ bool TellStick::isOpen() const {
 	return d->open;
 }
 
+bool TellStick::isSameAsDescriptor(const TellStickDescriptor &td) const {
+	if (td.vid != d->vid) {
+		return false;
+	}
+	if (td.pid != d->pid) {
+		return false;
+	}
+	if (td.serial != d->serial) {
+		return false;
+	}
+	return true;
+}
+
+void TellStick::run() {
+	//TODO
+}
+
 int TellStick::send( const std::string &message ) {
 	return TELLSTICK_SUCCESS;
 }
@@ -72,17 +97,43 @@ std::list<TellStickDescriptor> TellStick::findAll() {
 	}
 
 	return tellstick;
-	
+
 }
 
-std::list<TellStickDescriptor> TellStick::findAllByVIDPID( int vid, int pid ) {
-	std::list<TellStickDescriptor> retval;
-	
+bool TellStick::stillConnected() const {
 	ftdi_context ftdic;
 	struct ftdi_device_list *devlist, *curdev;
 	char serialBuffer[10];
 	ftdi_init(&ftdic);
-	
+	bool found = false;
+
+	int ret = ftdi_usb_find_all(&ftdic, &devlist, d->vid, d->pid);
+	if (ret > 0) {
+		for (curdev = devlist; curdev != NULL; curdev = curdev->next) {
+			ret = ftdi_usb_get_strings(&ftdic, curdev->dev, NULL, 0, NULL, 0, serialBuffer, 10);
+			if (ret != 0) {
+				continue;
+			}
+			if (d->serial.compare(serialBuffer) == 0) {
+				found = true;
+				break;
+			}
+		}
+	}
+
+	ftdi_list_free(&devlist);
+	ftdi_deinit(&ftdic);
+	return found;
+}
+
+std::list<TellStickDescriptor> TellStick::findAllByVIDPID( int vid, int pid ) {
+	std::list<TellStickDescriptor> retval;
+
+	ftdi_context ftdic;
+	struct ftdi_device_list *devlist, *curdev;
+	char serialBuffer[10];
+	ftdi_init(&ftdic);
+
 	int ret = ftdi_usb_find_all(&ftdic, &devlist, vid, pid);
 	if (ret > 0) {
 		for (curdev = devlist; curdev != NULL; curdev = curdev->next) {
