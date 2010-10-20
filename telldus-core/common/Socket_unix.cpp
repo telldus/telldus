c94afb760e1eb9a00b158e7d6d075a7410922b00
Micke Prag <micke.prag@telldus.se>
2010-10-20 14:51:43 +0000

    Made socket compile under Linux. Some functions still need to be implemented

--- telldus-core/common/Socket_unix.cpp
+++ telldus-core/common/Socket_unix.cpp
@@ -15,17 +15,20 @@ int connectWrapper(int sockfd, const struct sockaddr *addr, socklen_t addrlen) {
 class Socket::PrivateData {
 public:
 	SOCKET_T socket;
+	bool connected;
 };
 
 Socket::Socket() {
 	d = new PrivateData;
 	d->socket = 0;
+	d->connected = false;
 }
 
 Socket::Socket(SOCKET_T socket)
 {
 	d = new PrivateData;
 	d->socket = socket;
+	d->connected = true;
 }
 
 Socket::~Socket(void) {
@@ -35,31 +38,42 @@ Socket::~Socket(void) {
 void Socket::connect(const std::wstring &server) {
 	struct sockaddr_un remote;
 	socklen_t len;
-	
+
 	if ((d->socket = socket(AF_UNIX, SOCK_STREAM, 0)) == -1) {
 		return;
 	}
 	std::string name = "/tmp/" + std::string(server.begin(), server.end());
 	remote.sun_family = AF_UNIX;
 	strcpy(remote.sun_path, name.c_str());
-	
+
 	len = SUN_LEN(&remote);
 	if (connectWrapper(d->socket, (struct sockaddr *)&remote, len) == -1) {
 		return;
 	}
+	d->connected = true;
+}
+
+bool Socket::isConnected(){
+	return d->connected;
 }
 
 std::wstring Socket::read() {
+	//TODO set d->conneted to false if something goes wrong
 	char inbuf[BUFSIZE];
 	memset(inbuf, '\0', sizeof(inbuf));
-	
+
 	recv(d->socket, inbuf, BUFSIZE - 1, 0);
 
 	std::string msg(inbuf);
 	return std::wstring(msg.begin(), msg.end());
 }
 
+void Socket::stopReadWait(){
+	//TODO:
+}
+
 void Socket::write(const std::wstring &msg) {
+	//TODO set d->conneted to false if something goes wrong
 	std::string newMsg(msg.begin(), msg.end());
 	send(d->socket, newMsg.c_str(), newMsg.length(), 0);
 }
