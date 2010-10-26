617df3b497531b7cd4e75e7bfa56761b1fbb6e05
Micke Prag <micke.prag@telldus.se>
2010-10-26 14:36:05 +0000

    The macro is named WIN32, not WINDOWS

--- telldus-gui/cmake/FindQCA.cmake
+++ telldus-gui/cmake/FindQCA.cmake
@@ -1,15 +1,15 @@
 
 FIND_LIBRARY( QCA_LIBRARY qca )
 
-IF (WINDOWS)
+IF (WIN32)
 	GET_FILENAME_COMPONENT(QCA_PATH ${QCA_LIBRARY} PATH)
 	MESSAGE(${QCA_PATH})
 	INCLUDE_DIRECTORIES(
 		${QCA_PATH}/../include/QtCrypto/
 	)
 
-ELSE(WINDOWS)
+ELSE(WIN32)
 	#Maybe not so pretty, should be done better
 	INCLUDE_DIRECTORIES( /usr/include/qca2/QtCrypto/ )
 
-ENDIF (WINDOWS)
+ENDIF (WIN32)
