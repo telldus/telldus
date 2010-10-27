aad55f53078f682f65db845f6608cebb628901ba
Micke Prag <micke.prag@telldus.se>
2010-10-27 14:31:47 +0000

    The lock in send was not properly locked

--- telldus-core/service/TellStick_ftd2xx.cpp
+++ telldus-core/service/TellStick_ftd2xx.cpp
@@ -1,7 +1,7 @@
 //
 // C++ Implementation: TellStick
 //
-// Description: 
+// Description:
 //
 //
 // Author: Micke Prag <micke.prag@telldus.se>, (C) 2009
@@ -14,6 +14,7 @@
 #include "Strings.h"
 #include "../client/telldus-core.h"
 #include <string.h>
+#include <stdlib.h>
 
 #include "ftd2xx.h"
 
@@ -36,7 +37,7 @@ public:
 #endif
 };
 
-TellStick::TellStick(int controllerId, Event *event, const TellStickDescriptor &td ) 
+TellStick::TellStick(int controllerId, Event *event, const TellStickDescriptor &td )
 	:Controller(controllerId, event)
 {
 	d = new PrivateData;
@@ -67,12 +68,12 @@ TellStick::TellStick(int controllerId, Event *event, const TellStickDescriptor &
 		FT_SetFlowControl(d->ftHandle, FT_FLOW_NONE, 0, 0);
 		FT_SetTimeouts(d->ftHandle,5000,0);
 	}
-	
+
 	if (d->open) {
 		if (td.pid == 0x0C31) {
 			setBaud(9600);
 		} else {
- 			setBaud(4800);
+			setBaud(4800);
 		}
 		this->start();
 	}
@@ -152,7 +153,7 @@ void TellStick::run() {
 		pthread_cond_wait(&d->eh.eCondVar, &d->eh.eMutex);
 		pthread_mutex_unlock(&d->eh.eMutex);
 #endif
-		
+
 		TelldusCore::MutexLocker locker(&d->mutex);
 		if (!d->running) {
 			break;
@@ -179,7 +180,7 @@ int TellStick::send( const std::string &strMessage ) {
 	//This lock does two things
 	// 1 Prevents two calls from different threads to this function
 	// 2 Prevents our running thread from receiving the data we are interested in here
-	TelldusCore::MutexLocker(&d->mutex);
+	TelldusCore::MutexLocker locker(&d->mutex);
 
 	char *tempMessage = (char *)malloc(sizeof(char) * (strMessage.size()+1));
 #ifdef _WINDOWS
@@ -255,12 +256,12 @@ std::list<TellStickDescriptor> TellStick::findAll() {
 	}
 
 	return tellstick;
-	
+
 }
 
 std::list<TellStickDescriptor> TellStick::findAllByVIDPID( int vid, int pid ) {
 	std::list<TellStickDescriptor> retval;
-	
+
 	FT_HANDLE fthHandle = 0;
 	FT_STATUS ftStatus = FT_OK;
 	DWORD dwNumberOfDevices = 0;
@@ -273,20 +274,20 @@ std::list<TellStickDescriptor> TellStick::findAllByVIDPID( int vid, int pid ) {
 	if (ftStatus != FT_OK) {
 		return retval;
 	}
-	for (int i = 0; i < (int)dwNumberOfDevices; i++) { 
+	for (int i = 0; i < (int)dwNumberOfDevices; i++) {
 		FT_PROGRAM_DATA pData;
-		char ManufacturerBuf[32]; 
-		char ManufacturerIdBuf[16]; 
-		char DescriptionBuf[64]; 
-		char SerialNumberBuf[16]; 
+		char ManufacturerBuf[32];
+		char ManufacturerIdBuf[16];
+		char DescriptionBuf[64];
+		char SerialNumberBuf[16];
 
 		pData.Signature1 = 0x00000000;
 		pData.Signature2 = 0xffffffff;
 		pData.Version = 0x00000002;      // EEPROM structure with FT232R extensions
-		pData.Manufacturer = ManufacturerBuf; 
+		pData.Manufacturer = ManufacturerBuf;
 		pData.ManufacturerId = ManufacturerIdBuf;
-		pData.Description = DescriptionBuf; 
-		pData.SerialNumber = SerialNumberBuf; 
+		pData.Description = DescriptionBuf;
+		pData.SerialNumber = SerialNumberBuf;
 
 		ftStatus = FT_Open(i, &fthHandle);
 		ftStatus = FT_EE_Read(fthHandle, &pData);
