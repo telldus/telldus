c019caa01372af9ca2b3be5b978dd24026f664d2
Stefan Persson <stefan.persson@telldus.se>
2010-10-29 13:39:06 +0000

    Splash updated

--- scheduler/DeviceScheduler/frmSplash.Designer.cs
+++ scheduler/DeviceScheduler/frmSplash.Designer.cs
@@ -28,64 +28,64 @@ namespace DeviceScheduler
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmSplash));
-            this.label3 = new System.Windows.Forms.Label();
-            this.label1 = new System.Windows.Forms.Label();
-            this.timer1 = new System.Windows.Forms.Timer(this.components);
-            this.lblVersion = new System.Windows.Forms.Label();
-            this.SuspendLayout();
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.BackColor = System.Drawing.Color.Transparent;
-            this.label3.Location = new System.Drawing.Point(114, 280);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(341, 13);
-            this.label3.TabIndex = 6;
-            this.label3.Text = "© Copyright Telldus Technologies AB 2007. Alla rättigheter förbehålles.";
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.BackColor = System.Drawing.Color.Transparent;
-            this.label1.Location = new System.Drawing.Point(242, 195);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(45, 13);
-            this.label1.TabIndex = 7;
-            this.label1.Text = "Version:";
-            // 
-            // timer1
-            // 
-            this.timer1.Interval = 1500;
-            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
-            // 
-            // lblVersion
-            // 
-            this.lblVersion.AutoSize = true;
-            this.lblVersion.BackColor = System.Drawing.Color.Transparent;
-            this.lblVersion.Location = new System.Drawing.Point(293, 195);
-            this.lblVersion.Name = "lblVersion";
-            this.lblVersion.Size = new System.Drawing.Size(45, 13);
-            this.lblVersion.TabIndex = 8;
-            this.lblVersion.Text = "Version:";
-            // 
-            // frmSplash
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
-            this.ClientSize = new System.Drawing.Size(522, 300);
-            this.Controls.Add(this.lblVersion);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.label3);
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
-            this.Name = "frmSplash";
-            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Load += new System.EventHandler(this.frmSplash_Load);
-            this.ResumeLayout(false);
-            this.PerformLayout();
+			this.components = new System.ComponentModel.Container();
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmSplash));
+			this.label3 = new System.Windows.Forms.Label();
+			this.label1 = new System.Windows.Forms.Label();
+			this.timer1 = new System.Windows.Forms.Timer(this.components);
+			this.lblVersion = new System.Windows.Forms.Label();
+			this.SuspendLayout();
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.BackColor = System.Drawing.Color.Transparent;
+			this.label3.Location = new System.Drawing.Point(114, 280);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(341, 13);
+			this.label3.TabIndex = 6;
+			this.label3.Text = "© Copyright Telldus Technologies AB 2010. Alla rättigheter förbehålles.";
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.BackColor = System.Drawing.Color.Transparent;
+			this.label1.Location = new System.Drawing.Point(242, 195);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(45, 13);
+			this.label1.TabIndex = 7;
+			this.label1.Text = "Version:";
+			// 
+			// timer1
+			// 
+			this.timer1.Interval = 1500;
+			this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+			// 
+			// lblVersion
+			// 
+			this.lblVersion.AutoSize = true;
+			this.lblVersion.BackColor = System.Drawing.Color.Transparent;
+			this.lblVersion.Location = new System.Drawing.Point(293, 195);
+			this.lblVersion.Name = "lblVersion";
+			this.lblVersion.Size = new System.Drawing.Size(45, 13);
+			this.lblVersion.TabIndex = 8;
+			this.lblVersion.Text = "Version:";
+			// 
+			// frmSplash
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+			this.ClientSize = new System.Drawing.Size(522, 300);
+			this.Controls.Add(this.lblVersion);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.label3);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+			this.Name = "frmSplash";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+			this.Load += new System.EventHandler(this.frmSplash_Load);
+			this.ResumeLayout(false);
+			this.PerformLayout();
 
         }
 
