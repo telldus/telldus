c6e8caec7356d32aa4e8caf7eeb45b55adbe61e9
Micke Prag <micke.prag@telldus.se>
2011-01-12 12:09:12 +0000

    Added functions Device::pushCommand() and Device::popCommand()

--- telldus-gui/Plugins/TelldusCore/Device.js
+++ telldus-gui/Plugins/TelldusCore/Device.js
@@ -1,4 +1,20 @@
 function Device(id) {
 	this.id = id;
 	this.name = com.telldus.core.getName(id);
+	this.commandStack = new Array();
+}
+
+Device.prototype.pushCommand = function(command) {
+	this.commandStack.push(command);
+	command.execute(this.id);
+}
+
+Device.prototype.popCommand = function(command) {
+
+	this.commandStack.pop();
+	if (this.commandStack.length) {
+		var command = this.commandStack[this.commandStack.length-1].execute(this.id);
+	} else {
+		com.telldus.core.turnOff(this.id);
+	}
 }
