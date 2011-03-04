8f937294c3be621dd4ec239e6cf34aefa4b78593
Micke Prag <micke.prag@telldus.se>
2011-03-04 09:56:43 +0000

    Updated swedish translations for telldus-gui

--- telldus-gui/TelldusGui/TelldusGui_sv.ts
+++ telldus-gui/TelldusGui/TelldusGui_sv.ts
@@ -4,39 +4,54 @@
 <context>
     <name>DeviceModel</name>
     <message>
-        <location filename="devicemodel.cpp" line="43"/>
+        <location filename="devicemodel.cpp" line="69"/>
         <source>on</source>
         <translation>på</translation>
     </message>
     <message>
-        <location filename="devicemodel.cpp" line="45"/>
+        <location filename="devicemodel.cpp" line="71"/>
         <source>off</source>
         <translation>av</translation>
     </message>
     <message>
-        <location filename="devicemodel.cpp" line="48"/>
+        <location filename="devicemodel.cpp" line="73"/>
+        <source>up</source>
+        <translation>upp</translation>
+    </message>
+    <message>
+        <location filename="devicemodel.cpp" line="75"/>
+        <source>down</source>
+        <translation>ner</translation>
+    </message>
+    <message>
+        <location filename="devicemodel.cpp" line="77"/>
+        <source>stopped</source>
+        <translation>stoppad</translation>
+    </message>
+    <message>
+        <location filename="devicemodel.cpp" line="80"/>
         <source>%1%</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="devicemodel.cpp" line="50"/>
+        <location filename="devicemodel.cpp" line="82"/>
         <source>unknown %1</source>
-        <translation>okänd %1</translation>
+        <translation>okänt %1</translation>
     </message>
     <message>
-        <location filename="devicemodel.cpp" line="92"/>
+        <location filename="devicemodel.cpp" line="114"/>
         <source>State</source>
-        <translation>Läge</translation>
+        <translation>Status</translation>
     </message>
     <message>
-        <location filename="devicemodel.cpp" line="94"/>
+        <location filename="devicemodel.cpp" line="116"/>
         <source>Device name</source>
         <translation>Enhetsnamn</translation>
     </message>
     <message>
-        <location filename="devicemodel.cpp" line="96"/>
+        <location filename="devicemodel.cpp" line="118"/>
         <source>Action</source>
-        <translation>Styr</translation>
+        <translation>Utför</translation>
     </message>
 </context>
 <context>
@@ -54,7 +69,7 @@
     <message>
         <location filename="devicesettingarctechselflearning.cpp" line="59"/>
         <source>Randomize</source>
-        <translation type="unfinished"></translation>
+        <translation>Slumpa</translation>
     </message>
 </context>
 <context>
@@ -62,7 +77,7 @@
     <message>
         <location filename="devicesettinggao.cpp" line="28"/>
         <source>Unitcode</source>
-        <translation type="unfinished">Enhetskod</translation>
+        <translation>Enhetskod</translation>
     </message>
 </context>
 <context>
@@ -115,6 +130,27 @@
     </message>
 </context>
 <context>
+    <name>DeviceSettingSelflearning</name>
+    <message>
+        <location filename="devicesettingselflearning.cpp" line="33"/>
+        <source>Remote code</source>
+        <translation>Fjärrkontrollskod</translation>
+    </message>
+    <message>
+        <location filename="devicesettingselflearning.cpp" line="46"/>
+        <source>Randomize</source>
+        <translation>Slumpa</translation>
+    </message>
+</context>
+<context>
+    <name>DeviceSettingUnitcode</name>
+    <message>
+        <location filename="devicesettingunitcode.cpp" line="28"/>
+        <source>Unit code</source>
+        <translation>Enhetskod</translation>
+    </message>
+</context>
+<context>
     <name>DeviceSettingUpm</name>
     <message>
         <location filename="devicesettingupm.cpp" line="27"/>
@@ -135,88 +171,127 @@
 <context>
     <name>DeviceWidget</name>
     <message>
-        <location filename="devicewidget.cpp" line="39"/>
+        <location filename="devicewidget.cpp" line="40"/>
         <source>New device...</source>
         <translation>Ny enhet...</translation>
     </message>
     <message>
-        <location filename="devicewidget.cpp" line="41"/>
+        <location filename="devicewidget.cpp" line="42"/>
         <source>New group...</source>
         <translation>Ny grupp...</translation>
     </message>
     <message>
-        <location filename="devicewidget.cpp" line="45"/>
+        <location filename="devicewidget.cpp" line="46"/>
         <source>New</source>
         <translation>Ny</translation>
     </message>
     <message>
-        <location filename="devicewidget.cpp" line="55"/>
+        <location filename="devicewidget.cpp" line="56"/>
         <source>Edit</source>
         <translation>Redigera</translation>
     </message>
     <message>
-        <location filename="devicewidget.cpp" line="63"/>
+        <location filename="devicewidget.cpp" line="64"/>
         <source>Remove</source>
-        <translation>Ta bort</translation>
+        <translation>Fjärrkontrollskod</translation>
     </message>
     <message>
-        <location filename="devicewidget.cpp" line="119"/>
+        <location filename="devicewidget.cpp" line="131"/>
         <source>Are you sure you want to remove the selected device?</source>
-        <translation>Är du säker på att du vill ta bort den valda enheten?</translation>
+        <translation>Är du säker på att du vill ta bort vald enhet?</translation>
     </message>
     <message>
-        <location filename="devicewidget.cpp" line="120"/>
+        <location filename="devicewidget.cpp" line="132"/>
         <source>The device will be removed permanently from this application as well as all other applications.</source>
-        <translation>Enheten kommer att permanent tas bort från detta och alla andra program.</translation>
+        <translation>Enheten kommer tas bort permanent från både detta program såväl som alla andra program.</translation>
     </message>
 </context>
 <context>
     <name>EditDeviceDialog</name>
     <message>
-        <location filename="editdevicedialog.cpp" line="62"/>
+        <location filename="editdevicedialog.cpp" line="72"/>
+        <location filename="editdevicedialog.cpp" line="81"/>
+        <source>Scan</source>
+        <translation>Scanna</translation>
+    </message>
+    <message>
+        <location filename="editdevicedialog.cpp" line="75"/>
+        <source>Stop</source>
+        <translation>Stoppa</translation>
+    </message>
+    <message>
+        <location filename="editdevicedialog.cpp" line="103"/>
         <source>Device</source>
         <translation>Enhet</translation>
     </message>
     <message>
-        <location filename="editdevicedialog.cpp" line="75"/>
+        <location filename="editdevicedialog.cpp" line="116"/>
         <source>&amp;Name:</source>
         <translation>&amp;Namn:</translation>
     </message>
     <message>
-        <location filename="editdevicedialog.cpp" line="83"/>
+        <location filename="editdevicedialog.cpp" line="124"/>
         <source>Choose a device above</source>
         <translation>Välj en enhet ovan</translation>
     </message>
     <message>
-        <location filename="editdevicedialog.cpp" line="89"/>
+        <location filename="editdevicedialog.cpp" line="130"/>
         <source>Addresscode</source>
         <translation>Adresskod</translation>
     </message>
     <message>
-        <location filename="editdevicedialog.cpp" line="156"/>
+        <location filename="editdevicedialog.cpp" line="212"/>
+        <source>Please press a button on your remote</source>
+        <translation>Tryck en knapp på din fjärrkontroll</translation>
+    </message>
+    <message>
+        <location filename="editdevicedialog.cpp" line="219"/>
+        <source>If you have a TellStick Duo connected,&lt;br&gt;you can use it to scan the code of the remote control</source>
+        <translation>Om du har en TellStick Duo ansluten,&lt;br&gt;kan du använda den för att hitta rätt enhet och inställningar</translation>
+    </message>
+    <message>
+        <location filename="editdevicedialog.cpp" line="231"/>
         <source>You must choose a device</source>
         <translation>Du måste välja en enhet</translation>
     </message>
     <message>
-        <location filename="editdevicedialog.cpp" line="157"/>
+        <location filename="editdevicedialog.cpp" line="232"/>
         <source>Please select the device you have.</source>
         <translation>Välj den enhet du har.</translation>
     </message>
     <message>
-        <location filename="editdevicedialog.cpp" line="173"/>
+        <location filename="editdevicedialog.cpp" line="248"/>
         <source>The device must have a name.</source>
         <translation>Enheten måste ha ett namn.</translation>
     </message>
     <message>
-        <location filename="editdevicedialog.cpp" line="174"/>
+        <location filename="editdevicedialog.cpp" line="249"/>
         <source>Please fill in a name in the field under &apos;Name&apos;</source>
-        <translation>Fyll i ett namn i rutan &apos;Namn&apos;</translation>
+        <translation>Skriv in ett namn i fältet &apos;Namn&apos;</translation>
+    </message>
+</context>
+<context>
+    <name>EditGroupDialog</name>
+    <message>
+        <location filename="editgroupdialog.cpp" line="58"/>
+        <source>&amp;Name:</source>
+        <translation>&amp;Namn:</translation>
+    </message>
+    <message>
+        <location filename="editgroupdialog.cpp" line="165"/>
+        <source>The device must have a name.</source>
+        <translation>Enheten måste ha ett namn.</translation>
+    </message>
+    <message>
+        <location filename="editgroupdialog.cpp" line="166"/>
+        <source>Please fill in a name in the field under &apos;Name&apos;</source>
+        <translation>Skriv in ett namn i fältet &apos;Namn&apos;</translation>
     </message>
 </context>
 <context>
     <name>MethodWidget</name>
     <message>
-        <location filename="methodwidget.cpp" line="36"/>
+        <location filename="methodwidget.cpp" line="84"/>
         <source>Learn</source>
         <translation>Lär upp</translation>
     </message>
@@ -226,7 +301,7 @@
     <message>
         <location filename="vendordevicetreeitem.cpp" line="123"/>
         <source>Not a Telldus device-file</source>
-        <translation>Inte en Telldus-enhets-fil</translation>
+        <translation>Inte en Telldus enhetsfil</translation>
     </message>
 </context>
 <context>
