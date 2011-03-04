5c9f2c37fcffbe0d01c675cf6605d0676fe7e50e
Micke Prag <micke.prag@telldus.se>
2011-03-04 10:02:37 +0000

    Updated swedish translations for TelldusCenter

--- telldus-gui/TelldusCenter/TelldusCenter_sv.ts
+++ telldus-gui/TelldusCenter/TelldusCenter_sv.ts
@@ -4,50 +4,54 @@
 <context>
     <name>MainWindow</name>
     <message>
-        <location filename="mainwindow.cpp" line="59"/>
-        <source>Telldus Center</source>
+        <location filename="mainwindow.cpp" line="66"/>
+        <source>TelldusCenter</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="80"/>
+        <location filename="mainwindow.cpp" line="87"/>
         <source>&amp;File</source>
         <translation>&amp;Arkiv</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="81"/>
+        <location filename="mainwindow.cpp" line="88"/>
+        <source>&amp;Configure</source>
+        <translation>&amp;Inställningar</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="90"/>
         <source>&amp;Quit</source>
         <translation>&amp;Avsluta</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="84"/>
+        <location filename="mainwindow.cpp" line="94"/>
         <source>&amp;Help</source>
         <translation>&amp;Hjälp</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="86"/>
+        <location filename="mainwindow.cpp" line="96"/>
         <source>About &amp;Qt</source>
         <translation>Om &amp;Qt</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="87"/>
-        <source>About &amp;Telldus Center</source>
-        <translation>Om &amp;Telldus Center</translation>
+        <location filename="mainwindow.cpp" line="98"/>
+        <source>About &amp;TelldusCenter</source>
+        <translation>Om &amp;TelldusCenter</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="93"/>
+        <location filename="mainwindow.cpp" line="105"/>
         <source>Pages</source>
         <translation>Sidor</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="122"/>
-        <source>About Telldus Center</source>
-        <translation>Om Telldus Center</translation>
+        <location filename="mainwindow.cpp" line="134"/>
+        <source>About TelldusCenter</source>
+        <translation>Om TelldusCenter</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="123"/>
-        <source>&lt;center&gt;&lt;img src=&apos;:/images/TelldusCenter_128.png&apos; width=128 height=128 /&gt;&lt;h2&gt;Telldus Center 2.0.0_beta2&lt;/h2&gt;&lt;p&gt;Copyright &amp;copy; 2009 Telldus Technologies AB&lt;p&gt;&lt;/center&gt;</source>
-        <oldsource>&lt;center&gt;&lt;img src=&apos;:/images/TelldusCenter_128.png&apos; width=128 height=128 /&gt;&lt;h2&gt;Telldus Center 2.0.0_beta1&lt;/h2&gt;&lt;p&gt;Copyright &amp;copy; 2009 Telldus Technologies AB&lt;p&gt;&lt;/center&gt;</oldsource>
-        <translation type="unfinished"></translation>
+        <location filename="mainwindow.cpp" line="135"/>
+        <source>&lt;center&gt;&lt;img src=&apos;:/images/TelldusCenter_128.png&apos; width=128 height=128 /&gt;&lt;h2&gt;TelldusCenter %1&lt;/h2&gt;&lt;p&gt;Copyright &amp;copy; 2010 Telldus Technologies AB&lt;p&gt;&lt;/center&gt;</source>
+        <translation></translation>
     </message>
 </context>
 </TS>
