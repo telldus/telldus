09db7743838bb869785e6ed70eff6aac0e43ddc1
Stefan Persson <stefan.persson@telldus.se>
2011-09-01 07:31:06 +0000

    Corrected information in Assembly File

--- bindings/dotnet/TelldusNETWrapper/Properties/AssemblyInfo.cs
+++ bindings/dotnet/TelldusNETWrapper/Properties/AssemblyInfo.cs
@@ -5,19 +5,19 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ClassLibrary1")]
+[assembly: AssemblyTitle("TelldusNETWrapper")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("ClassLibrary1")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2010")]
+[assembly: AssemblyCompany("Telldus Technologies AB")]
+[assembly: AssemblyProduct("")]
+[assembly: AssemblyCopyright("Copyright © Telldus Technologies AB 2010")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
+[assembly: ComVisible(true)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("a66028f7-b342-4dc0-9504-9e07c462ec15")]
