e89d0a29f8238b64c4110c803126ffad8625a5ea
Stefan Persson <stefan.persson@telldus.se>
2011-09-23 12:21:21 +0000

    Possible to specify a path to tdadmin in the udev.sh script

--- telldus-core/tdadmin/udev.sh
+++ telldus-core/tdadmin/udev.sh
@@ -2,8 +2,8 @@
 
 if [ "${ID_VENDOR_ID}" == "1781" ]; then
 	if [ "${ACTION}" == "add" ]; then
-		tdadmin controller connect --pid=${ID_MODEL_ID} --vid=${ID_VENDOR_ID} --serial=${ID_SERIAL_SHORT}
+		@TDADMIN_PATH@tdadmin controller connect --pid=${ID_MODEL_ID} --vid=${ID_VENDOR_ID} --serial=${ID_SERIAL_SHORT}
 	elif [ "${ACTION}" == "remove" ]; then
-		tdadmin controller disconnect --pid=${ID_MODEL_ID} --vid=${ID_VENDOR_ID} --serial=${ID_SERIAL_SHORT}
+		@TDADMIN_PATH@tdadmin controller disconnect --pid=${ID_MODEL_ID} --vid=${ID_VENDOR_ID} --serial=${ID_SERIAL_SHORT}
 	fi
 fi
