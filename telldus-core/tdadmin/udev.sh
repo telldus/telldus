4ee0542c11a861e4734bdc91e5f91c04d0e9bc72
Stefan Persson <stefan.persson@telldus.se>
2011-10-04 11:59:36 +0000

    Set tdadmin path in script automatically

--- telldus-core/tdadmin/udev.sh
+++ telldus-core/tdadmin/udev.sh
@@ -2,8 +2,8 @@
 
 if [ "${ID_VENDOR_ID}" == "1781" ]; then
 	if [ "${ACTION}" == "add" ]; then
-		@TDADMIN_PATH@tdadmin controller connect --pid=${ID_MODEL_ID} --vid=${ID_VENDOR_ID} --serial=${ID_SERIAL_SHORT}
+		@CMAKE_INSTALL_PREFIX@/sbin/tdadmin controller connect --pid=${ID_MODEL_ID} --vid=${ID_VENDOR_ID} --serial=${ID_SERIAL_SHORT}
 	elif [ "${ACTION}" == "remove" ]; then
-		@TDADMIN_PATH@tdadmin controller disconnect --pid=${ID_MODEL_ID} --vid=${ID_VENDOR_ID} --serial=${ID_SERIAL_SHORT}
+		@CMAKE_INSTALL_PREFIX@/sbin/tdadmin controller disconnect --pid=${ID_MODEL_ID} --vid=${ID_VENDOR_ID} --serial=${ID_SERIAL_SHORT}
 	fi
 fi
