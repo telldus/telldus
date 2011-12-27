83fb477ef48ee16575dfdabd9212625eb2d52be8
Micke Prag <micke.prag@telldus.se>
2011-12-27 15:46:49 +0100

    Use the log-system in main_unix.cpp

--- telldus-core/service/main_unix.cpp
+++ telldus-core/service/main_unix.cpp
@@ -4,7 +4,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <syslog.h>
 #include <sys/stat.h>
 #include <errno.h>
 #include <pwd.h>
@@ -12,6 +11,7 @@
 
 #include "Settings.h"
 #include "Strings.h"
+#include "Log.h"
 
 #define DAEMON_NAME "telldusd"
 #define PID_FILE "/var/run/" DAEMON_NAME ".pid"
@@ -21,19 +21,19 @@ TelldusMain tm;
 void signalHandler(int sig) {
 	switch(sig) {
 		case SIGHUP:
-			syslog(LOG_WARNING, "Received SIGHUP signal.");
+			Log::warning("Received SIGHUP signal.");
 			break;
 		case SIGTERM:
 		case SIGINT:
-			syslog(LOG_WARNING, "Received SIGTERM or SIGINT signal.");
-			syslog(LOG_WARNING, "Shutting down");
+			Log::warning("Received SIGTERM or SIGINT signal.");
+			Log::warning("Shutting down");
 			tm.stop();
 			break;
 		case SIGPIPE:
-			syslog(LOG_WARNING, "Received SIGPIPE signal.");
+			Log::warning("Received SIGPIPE signal.");
 			break;
 		default:
-			syslog(LOG_WARNING, "Unhandled signal (%d) %s", sig, strsignal(sig));
+			Log::warning("Unhandled signal (%d) %s", sig, strsignal(sig));
 			break;
 	}
 }
@@ -46,6 +46,7 @@ int main(int argc, char **argv) {
 	for (int i = 1; i < argc; ++i) {
 		if (strcmp(argv[i], "--nodaemon") == 0) {
 			deamonize = false;
+			Log::setLogOutput(Log::StdOut);
 		} else if (strcmp(argv[i], "--help") == 0) {
 			printf("Telldus TellStick background service\n\nStart with --nodaemon to not run as daemon\n\n");
 			printf("Report bugs to <info.tech@telldus.com>\n");
@@ -77,17 +78,14 @@ int main(int argc, char **argv) {
 				fprintf(fd,"%d\n",pid);
 				fclose(fd);
 			} else {
-				syslog(LOG_ERR, "Could not open pid file %s: %s", PID_FILE, strerror(errno));
+				Log::error("Could not open pid file %s: %s", PID_FILE, strerror(errno));
 				exit(EXIT_FAILURE);
 			}
 			exit(EXIT_SUCCESS);
 		}
 	}
 
-	setlogmask(LOG_UPTO(LOG_INFO));
-	openlog(DAEMON_NAME, LOG_CONS, LOG_USER);
-
-	syslog(LOG_NOTICE, "%s daemon starting up", DAEMON_NAME);
+	Log::notice("%s daemon starting up", DAEMON_NAME);
 
 	if (deamonize) {
 		/* Change the file mode mask */
@@ -116,14 +114,14 @@ int main(int argc, char **argv) {
 		if (grp) {
 			setgid(grp->gr_gid);
 		} else {
-			syslog(LOG_WARNING, "Group %s could not be found", group.c_str());
+			Log::warning("Group %s could not be found", group.c_str());
 			exit(EXIT_FAILURE);
 		}
 		struct passwd *pw = getpwnam(user.c_str());
 		if (pw) {
 			setuid( pw->pw_uid );
 		} else {
-			syslog(LOG_WARNING, "User %s could not be found", user.c_str());
+			Log::warning("User %s could not be found", user.c_str());
 			exit(EXIT_FAILURE);
 		}
 	}
@@ -140,6 +138,6 @@ int main(int argc, char **argv) {
 
 	tm.start();
 
-	syslog(LOG_NOTICE, "%s daemon exited", DAEMON_NAME);
+	Log::notice("%s daemon exited", DAEMON_NAME);
 	exit(EXIT_SUCCESS);
 }
