a1809f410ac9b8434b174ee8910c6e8ef1bf9e27
Micke Prag <micke.prag@telldus.se>
2012-02-10 16:41:04 +0100

    This test seems to return undefined results

--- telldus-core/tests/common/StringsTest.cpp
+++ telldus-core/tests/common/StringsTest.cpp
@@ -13,7 +13,6 @@ void StringsTest :: tearDown (void)
 
 void StringsTest :: formatfTest (void) {
 	CPPUNIT_ASSERT_EQUAL(std::string("42"), TelldusCore::formatf("%u", 42));
-	CPPUNIT_ASSERT_EQUAL(std::string("0"), TelldusCore::formatf("%u", 42.1));
 	CPPUNIT_ASSERT_EQUAL(std::string("2A"), TelldusCore::formatf("%X", 42));
 	CPPUNIT_ASSERT_EQUAL(std::string("42"), TelldusCore::formatf("%s", "42"));
 }
