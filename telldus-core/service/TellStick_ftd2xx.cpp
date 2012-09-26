f5756e3f930c972ef9627e8b679f7d1863ea32be
Micke Prag <micke.prag@telldus.se>
2012-09-26 16:16:44 +0200

    Update TellStick_ftd2xx.cpp to our style guidelines

--- telldus-core/service/TellStick_ftd2xx.cpp
+++ telldus-core/service/TellStick_ftd2xx.cpp
@@ -9,17 +9,19 @@
 // Copyright: See COPYING file that comes with this distribution
 //
 //
-#include "TellStick.h"
-#include "common.h"
-#include "Mutex.h"
-#include "Settings.h"
-#include "Strings.h"
-#include "Log.h"
-#include "../client/telldus-core.h"
 #include <string.h>
 #include <stdlib.h>
+#include <string>
+#include <list>
+#include "common/common.h"
+#include "common/Mutex.h"
+#include "common/Strings.h"
+#include "service/Log.h"
+#include "service/Settings.h"
+#include "service/TellStick.h"
+#include "../client/telldus-core.h"
 
-#include "ftd2xx.h"
+#include "service/ftd2xx.h"
 
 class TellStick::PrivateData {
 public:
@@ -32,7 +34,7 @@ public:
 #ifdef _WINDOWS
 	HANDLE eh;
 #else
-//#include <unistd.h>
+// #include <unistd.h>
 	struct {
 		pthread_cond_t eCondVar;
 		pthread_mutex_t eMutex;
@@ -41,8 +43,7 @@ public:
 };
 
 TellStick::TellStick(int controllerId, TelldusCore::EventRef event, TelldusCore::EventRef updateEvent, const TellStickDescriptor &td )
-	:Controller(controllerId, event, updateEvent)
-{
+	:Controller(controllerId, event, updateEvent) {
 	d = new PrivateData;
 #ifdef _WINDOWS
 	d->eh = CreateEvent( NULL, false, false, NULL );
@@ -56,13 +57,13 @@ TellStick::TellStick(int controllerId, TelldusCore::EventRef event, TelldusCore:
 	d->pid = td.pid;
 	d->serial = td.serial;
 	Settings set;
-	d->ignoreControllerConfirmation = set.getSetting(L"ignoreControllerConfirmation")==L"true";
+	d->ignoreControllerConfirmation = set.getSetting(L"ignoreControllerConfirmation") == L"true";
 
 	char *tempSerial = new char[td.serial.size()+1];
 #ifdef _WINDOWS
 	strcpy_s(tempSerial, td.serial.size()+1, td.serial.c_str());
 #else
-	strcpy(tempSerial, td.serial.c_str());
+	snprintf(tempSerial, td.serial.size()+1, "%s", td.serial.c_str());
 	FT_SetVIDPID(td.vid, td.pid);
 #endif
 	Log::notice("Connecting to TellStick (%X/%X) with serial %s", d->vid, d->pid, d->serial.c_str());
@@ -71,7 +72,7 @@ TellStick::TellStick(int controllerId, TelldusCore::EventRef event, TelldusCore:
 	if (ftStatus == FT_OK) {
 		d->open = true;
 		FT_SetFlowControl(d->ftHandle, FT_FLOW_NONE, 0, 0);
-		FT_SetTimeouts(d->ftHandle,5000,0);
+		FT_SetTimeouts(d->ftHandle, 5000, 0);
 	}
 
 	if (d->open) {
@@ -139,29 +140,29 @@ bool TellStick::isSameAsDescriptor(const TellStickDescriptor &td) const {
 
 void TellStick::processData( const std::string &data ) {
 	for (unsigned int i = 0; i < data.length(); ++i) {
-		if (data[i] == 13) { // Skip \r
+		if (data[i] == 13) {  // Skip \r
 			continue;
-		} else if (data[i] == 10) { // \n found
-			if (d->message.substr(0,2).compare("+V") == 0) {
+		} else if (data[i] == 10) {  // \n found
+			if (d->message.substr(0, 2).compare("+V") == 0) {
 				setFirmwareVersion(TelldusCore::charToInteger(d->message.substr(2).c_str()));
-			} else if (d->message.substr(0,2).compare("+R") == 0) {
+			} else if (d->message.substr(0, 2).compare("+R") == 0) {
 				this->publishData(d->message.substr(2));
-			} else if(d->message.substr(0,2).compare("+W") == 0) {
+			} else if(d->message.substr(0, 2).compare("+W") == 0) {
 				this->decodePublishData(d->message.substr(2));
 			}
 			d->message.clear();
-		} else { // Append the character
+		} else {  // Append the character
 			d->message.append( 1, data[i] );
 		}
 	}
 }
 
-int TellStick::reset(){
+int TellStick::reset() {
 #ifndef _WINDOWS
-	return TELLSTICK_SUCCESS; //nothing to be done on other platforms
+	return TELLSTICK_SUCCESS;  // nothing to be done on other platforms
 #else
 	int success = FT_CyclePort( d->ftHandle );
-	if(success == FT_OK){
+	if(success == FT_OK) {
 		return TELLSTICK_SUCCESS;
 	}
 	return TELLSTICK_ERROR_UNKNOWN;
@@ -174,14 +175,14 @@ void TellStick::run() {
 	DWORD dwBytesRead = 0;
 	char *buf = 0;
 
-	//Send a firmware version request
+	// Send a firmware version request
 	char msg[] = "V+";
 	FT_Write(d->ftHandle, msg, (DWORD)strlen(msg), &dwBytesRead);
 
 	while(1) {
 #ifdef _WINDOWS
 		FT_SetEventNotification(d->ftHandle, FT_EVENT_RXCHAR, d->eh);
-		WaitForSingleObject(d->eh,INFINITE);
+		WaitForSingleObject(d->eh, INFINITE);
 #else
 		FT_SetEventNotification(d->ftHandle, FT_EVENT_RXCHAR, (PVOID)&d->eh);
 		pthread_mutex_lock(&d->eh.eMutex);
@@ -198,7 +199,7 @@ void TellStick::run() {
 			d->mutex.unlock();
 			continue;
 		}
-		buf = (char*)malloc(sizeof(buf) * (dwBytesInQueue+1));
+		buf = reinterpret_cast<char*>(malloc(sizeof(buf) * (dwBytesInQueue+1)));
 		memset(buf, 0, dwBytesInQueue+1);
 		FT_Read(d->ftHandle, buf, dwBytesInQueue, &dwBytesRead);
 		processData( buf );
@@ -210,17 +211,17 @@ int TellStick::send( const std::string &strMessage ) {
 	if (!d->open) {
 		return TELLSTICK_ERROR_NOT_FOUND;
 	}
-	
-	//This lock does two things
-	// 1 Prevents two calls from different threads to this function
-	// 2 Prevents our running thread from receiving the data we are interested in here
+
+	// This lock does two things
+	//  1 Prevents two calls from different threads to this function
+	//  2 Prevents our running thread from receiving the data we are interested in here
 	TelldusCore::MutexLocker locker(&d->mutex);
 
-	char *tempMessage = (char *)malloc(sizeof(char) * (strMessage.size()+1));
+	char *tempMessage = reinterpret_cast<char *>(malloc(sizeof(std::string::value_type) * (strMessage.size()+1)));
 #ifdef _WINDOWS
 	strcpy_s(tempMessage, strMessage.size()+1, strMessage.c_str());
 #else
-	strcpy(tempMessage, strMessage.c_str());
+	snprintf(tempMessage, strMessage.size()+1, "%s", strMessage.c_str());
 #endif
 
 	ULONG bytesWritten, bytesRead;
@@ -228,24 +229,24 @@ int TellStick::send( const std::string &strMessage ) {
 	FT_STATUS ftStatus;
 	ftStatus = FT_Write(d->ftHandle, tempMessage, (DWORD)strMessage.length(), &bytesWritten);
 	free(tempMessage);
-	
-	if(ftStatus != FT_OK){
+
+	if(ftStatus != FT_OK) {
 		Log::debug("Broken pipe on send");
 		return TELLSTICK_ERROR_BROKEN_PIPE;
 	}
 
-	if(strMessage.compare("N+") == 0 && ((pid() == 0x0C31 && firmwareVersion() < 5) || (pid() == 0x0C30 && firmwareVersion() < 6))){
-		//these firmware versions doesn't implement ack to noop, just check that the noop can be sent correctly
+	if(strMessage.compare("N+") == 0 && ((pid() == 0x0C31 && firmwareVersion() < 5) || (pid() == 0x0C30 && firmwareVersion() < 6))) {
+		// these firmware versions doesn't implement ack to noop, just check that the noop can be sent correctly
 		return TELLSTICK_SUCCESS;
 	}
-	if(d->ignoreControllerConfirmation){
-		//wait for TellStick to finish its air-sending
+	if(d->ignoreControllerConfirmation) {
+		// wait for TellStick to finish its air-sending
 		msleep(1000);
 		return TELLSTICK_SUCCESS;
 	}
 
 	while(1) {
-		ftStatus = FT_Read(d->ftHandle,&in,1,&bytesRead);
+		ftStatus = FT_Read(d->ftHandle, &in, 1, &bytesRead);
 		if (ftStatus == FT_OK) {
 			if (bytesRead == 1) {
 				if (in == '\n') {
@@ -253,10 +254,10 @@ int TellStick::send( const std::string &strMessage ) {
 				} else {
 					continue;
 				}
-			} else { //Timeout
+			} else {  // Timeout
 				return TELLSTICK_ERROR_COMMUNICATION;
 			}
-		} else { //Error
+		} else {  // Error
 			Log::debug("Broken pipe on read");
 			return TELLSTICK_ERROR_BROKEN_PIPE;
 		}
@@ -274,7 +275,7 @@ bool TellStick::stillConnected() const {
 	if (numDevs <= 0) {
 		return false;
 	}
-	for (int i = 0; i < (int)numDevs; i++) {
+	for (int i = 0; i < static_cast<int>(numDevs); i++) {
 		FT_HANDLE ftHandleTemp;
 		DWORD flags;
 		DWORD id;
@@ -303,7 +304,6 @@ std::list<TellStickDescriptor> TellStick::findAll() {
 	}
 
 	return tellstick;
-
 }
 
 std::list<TellStickDescriptor> TellStick::findAllByVIDPID( int vid, int pid ) {
@@ -323,8 +323,8 @@ std::list<TellStickDescriptor> TellStick::findAllByVIDPID( int vid, int pid ) {
 	if (dwNumberOfDevices > 0) {
 		FT_DEVICE_LIST_INFO_NODE *devInfo;
 		// allocate storage for list based on dwNumberOfDevices
-		devInfo = (FT_DEVICE_LIST_INFO_NODE*)malloc(sizeof(FT_DEVICE_LIST_INFO_NODE)*dwNumberOfDevices); // get the device information list 
-		ftStatus = FT_GetDeviceInfoList(devInfo,&dwNumberOfDevices);
+		devInfo = reinterpret_cast<FT_DEVICE_LIST_INFO_NODE*>(malloc(sizeof(FT_DEVICE_LIST_INFO_NODE)*dwNumberOfDevices));  // get the device information list
+		ftStatus = FT_GetDeviceInfoList(devInfo, &dwNumberOfDevices);
 		if (ftStatus == FT_OK) {
 			unsigned int id = (vid << 16) | pid;
 			for (unsigned int i = 0; i < dwNumberOfDevices; i++) {
