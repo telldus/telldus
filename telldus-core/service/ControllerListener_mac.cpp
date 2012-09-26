c52217329692e36069c6c0614e4455b617f12a1a
Micke Prag <micke.prag@telldus.se>
2012-09-26 16:31:27 +0200

    Update ControllerListener_mac.cpp to our style guidelines

--- telldus-core/service/ControllerListener_mac.cpp
+++ telldus-core/service/ControllerListener_mac.cpp
@@ -1,10 +1,17 @@
-#include "ControllerListener.h"
+//
+// Copyright (C) 2012 Telldus Technologies AB. All rights reserved.
+//
+// Copyright: See COPYING file that comes with this distribution
+//
+//
+#include "service/ControllerListener.h"
 
 #include <CoreFoundation/CoreFoundation.h>
 #include <IOKit/IOKitLib.h>
 #include <IOKit/IOMessage.h>
 #include <IOKit/IOCFPlugIn.h>
 #include <IOKit/usb/IOUSBLib.h>
+#include <string>
 
 class TellStickData {
 public:
@@ -22,15 +29,14 @@ public:
 	io_iterator_t			gAddedIter;
 	TelldusCore::EventRef				event;
 	bool running;
-	
+
 	void addUsbFilter(int vid, int pid);
 	static void DeviceAdded(void *refCon, io_iterator_t iterator);
 	static void DeviceNotification(void *refCon, io_service_t service, natural_t messageType, void *messageArgument);
 };
 
 ControllerListener::ControllerListener(TelldusCore::EventRef event)
-:Thread()
-{
+:Thread() {
 	d = new PrivateData;
 	d->event = event;
 	d->running = true;
@@ -48,135 +54,134 @@ ControllerListener::~ControllerListener() {
 }
 
 void ControllerListener::run() {
-    CFRunLoopSourceRef		runLoopSource;
-			
+	CFRunLoopSourceRef		runLoopSource;
+
 	d->gNotifyPort = IONotificationPortCreate(kIOMasterPortDefault);
-    runLoopSource = IONotificationPortGetRunLoopSource(d->gNotifyPort);
-    
+	runLoopSource = IONotificationPortGetRunLoopSource(d->gNotifyPort);
+
 	d->gRunLoop = CFRunLoopGetCurrent();
-    CFRunLoopAddSource(d->gRunLoop, runLoopSource, kCFRunLoopDefaultMode);
-    
+	CFRunLoopAddSource(d->gRunLoop, runLoopSource, kCFRunLoopDefaultMode);
+
 	d->addUsbFilter(0x1781, 0x0c30);
 	d->addUsbFilter(0x1781, 0x0c31);
-	
+
 	// Race check, if destructor was called really close to thread init,
 	// running might have gone false. Make sure we don't get stuck
-	if(d->running)
+	if (d->running) {
 		CFRunLoopRun();
+	}
 }
 
 void ControllerListener::PrivateData::addUsbFilter(int vid, int pid) {
-    CFNumberRef				numberRef;
+	CFNumberRef				numberRef;
 	kern_return_t			kr;
 	CFMutableDictionaryRef 	matchingDict;
 
-	matchingDict = IOServiceMatching(kIOUSBDeviceClassName);	// Interested in instances of class
-	                                                            // IOUSBDevice and its subclasses
+	matchingDict = IOServiceMatching(kIOUSBDeviceClassName);  // Interested in instances of class
+	                                                          // IOUSBDevice and its subclasses
 	if (matchingDict == NULL) {
 		return;
 	}
-	
+
 	// Create a CFNumber for the idVendor and set the value in the dictionary
 	numberRef = CFNumberCreate(kCFAllocatorDefault, kCFNumberSInt32Type, &vid);
 	CFDictionarySetValue(matchingDict, CFSTR(kUSBVendorID), numberRef);
 	CFRelease(numberRef);
-    
+
 	// Create a CFNumber for the idProduct and set the value in the dictionary
 	numberRef = CFNumberCreate(kCFAllocatorDefault, kCFNumberSInt32Type, &pid);
 	CFDictionarySetValue(matchingDict, CFSTR(kUSBProductID), numberRef);
 	CFRelease(numberRef);
-	
-	// Now set up a notification to be called when a device is first matched by I/O Kit.
-	kr = IOServiceAddMatchingNotification(gNotifyPort,					// notifyPort
-                                          kIOFirstMatchNotification,	// notificationType
-                                          matchingDict,					// matching
-                                          PrivateData::DeviceAdded,					// callback
-                                          this,							// refCon
-                                          &gAddedIter					// notification
-                                          );
-	// Iterate once to get already-present devices and arm the notification    
-	PrivateData::DeviceAdded(this, gAddedIter);	
 
+	// Now set up a notification to be called when a device is first matched by I/O Kit.
+	kr = IOServiceAddMatchingNotification(gNotifyPort,                // notifyPort
+	                                      kIOFirstMatchNotification,  // notificationType
+	                                      matchingDict,               // matching
+	                                      PrivateData::DeviceAdded,   // callback
+	                                      this,                       // refCon
+	                                      &gAddedIter                 // notification
+	                                      );
+	// Iterate once to get already-present devices and arm the notification
+	PrivateData::DeviceAdded(this, gAddedIter);
 }
 
 void ControllerListener::PrivateData::DeviceNotification(void *refCon, io_service_t service, natural_t messageType, void *messageArgument) {
 	kern_return_t	kr;
-    
-    if (messageType != kIOMessageServiceIsTerminated) {
+
+	if (messageType != kIOMessageServiceIsTerminated) {
 		return;
 	}
-	
+
 	TellStickData *tsd = reinterpret_cast<TellStickData*> (refCon);
 	if (!tsd) {
 		return;
 	}
-	
+
 	CFIndex size = CFStringGetLength(tsd->serialNumber);
 	char *s = new char[size+1];
 	CFStringGetCString(tsd->serialNumber, s, size+1, kCFStringEncodingASCII);
-	std::string serial(s); //Copy the string to the stack
+	std::string serial(s);  // Copy the string to the stack
 	delete[] s;
-	
+
 	ControllerChangeEventData *data = new ControllerChangeEventData;
 	data->vid = tsd->vid;
 	data->pid = tsd->pid;
 	data->inserted = false;
 	tsd->event->signal(data);
-	
+
 	// Free the data we're no longer using now that the device is going away
 	CFRelease(tsd->serialNumber);
-                
+
 	kr = IOObjectRelease(tsd->notification);
-        
+
 	delete tsd;
 }
 
 void ControllerListener::PrivateData::DeviceAdded(void *refCon, io_iterator_t iterator) {
-	io_service_t		usbDevice;
-	kern_return_t		kr;
-	
+	io_service_t usbDevice;
+	kern_return_t kr;
+
 	PrivateData *pd = reinterpret_cast<PrivateData*> (refCon);
-	
+
 	while ((usbDevice = IOIteratorNext(iterator))) {
 		TellStickData *tsd = new TellStickData;
 		tsd->event = pd->event;
-		
+
 		// Get the serial number
 		CFStringRef serialRef = reinterpret_cast<CFStringRef>(IORegistryEntryCreateCFProperty(  usbDevice, CFSTR("USB Serial Number" ), kCFAllocatorDefault, 0 ));
 		if (serialRef == NULL) {
-			//No serial number, we cannot continue. Sorry
+			// No serial number, we cannot continue. Sorry
 			continue;
 		}
-		
 
 		CFNumberRef vidRef = reinterpret_cast<CFNumberRef> (IORegistryEntryCreateCFProperty(usbDevice, CFSTR("idVendor"), kCFAllocatorDefault, 0));
 		if (vidRef) {
 			CFNumberGetValue(vidRef, kCFNumberIntType, &(tsd->vid));
 			CFRelease(vidRef);
 		}
-		
+
 		CFNumberRef pidRef = reinterpret_cast<CFNumberRef> (IORegistryEntryCreateCFProperty(usbDevice, CFSTR("idProduct"), kCFAllocatorDefault, 0));
 		if (pidRef) {
 			CFNumberGetValue(pidRef, kCFNumberIntType, &(tsd->pid));
 			CFRelease(pidRef);
 		}
-				
+
 		CFStringRef serialNumberAsCFString = CFStringCreateCopy(kCFAllocatorDefault, serialRef);
 		tsd->serialNumber = serialNumberAsCFString;
-		CFRelease(serialRef); 
-		
+		CFRelease(serialRef);
+
 		// Register for an interest notification of this device being removed. Use a reference to our
-        // private data as the refCon which will be passed to the notification callback.
-        kr = IOServiceAddInterestNotification(pd->gNotifyPort, usbDevice, kIOGeneralInterest, DeviceNotification, tsd, &(tsd->notification));
-		
+		// private data as the refCon which will be passed to the notification callback.
+		kr = IOServiceAddInterestNotification(pd->gNotifyPort, usbDevice, kIOGeneralInterest, DeviceNotification, tsd, &(tsd->notification));
+
 		CFIndex size = CFStringGetLength(serialNumberAsCFString);
 		char *s = new char[size+1];
 		CFStringGetCString(serialNumberAsCFString, s, size+1, kCFStringEncodingASCII);
-		std::string serial(s); //Copy the string to the stack
+		std::string serial(s);  // Copy the string to the stack
 		delete[] s;
-		
+
 		kr = IOObjectRelease(usbDevice);
-		
+
 		ControllerChangeEventData *data = new ControllerChangeEventData;
 		data->vid = tsd->vid;
 		data->pid = tsd->pid;
