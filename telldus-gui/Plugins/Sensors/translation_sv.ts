7372963c6d76646cec4eccb0f9b518b17ceaf32d
Micke Prag <micke.prag@telldus.se>
2013-02-14 16:02:31 +0100

    Add swedish translation for plugin Sensors, closes #163

---
+++ telldus-gui/Plugins/Sensors/translation_sv.ts
@@ -0,0 +1,69 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="sv_SE">
+<context>
+    <name>HeaderTitle</name>
+    <message>
+        <source>Name</source>
+        <translation>Namn</translation>
+    </message>
+</context>
+<context>
+    <name>SensorList</name>
+    <message>
+        <source>Name</source>
+        <translation>Namn</translation>
+    </message>
+    <message>
+        <source>ID</source>
+        <translation>ID</translation>
+    </message>
+    <message>
+        <source>Model</source>
+        <translation>Modell</translation>
+    </message>
+    <message>
+        <source>Visible in list</source>
+        <translation>Visas i lista</translation>
+    </message>
+    <message>
+        <source>Sensor information</source>
+        <translation>Sensorinformation</translation>
+    </message>
+    <message>
+        <source>Last updated</source>
+        <translation>Uppdaterad</translation>
+    </message>
+    <message>
+        <source>Edit</source>
+        <translation>Redigera</translation>
+    </message>
+    <message>
+        <source>View</source>
+        <translation>Visa</translation>
+    </message>
+</context>
+<context>
+    <name>SensorView</name>
+    <message>
+        <source>&lt;unnamed&gt;</source>
+        <translation>&lt;inget namn&gt;</translation>
+    </message>
+    <message>
+        <source>Enter a name</source>
+        <translation>Skriv in ett namn</translation>
+    </message>
+    <message>
+        <source>Confirm deletion</source>
+        <translation>Bekräfta borttagning</translation>
+    </message>
+    <message>
+        <source>Delete this sensor?</source>
+        <translation>Ta bort sensorn?</translation>
+    </message>
+    <message>
+        <source>Please note that a sensor that is still transmitting will reappear here again, but it will be hidden in the list by default.</source>
+        <translation>Notera att en sensor som fortfarande sänder kommer tillbaka automatiskt, dock dold som standard.</translation>
+    </message>
+</context>
+</TS>
