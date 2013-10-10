0886564e9445e8cacbe6374285b08211bffa10a0
Micke Prag <micke.prag@telldus.se>
2013-10-10 16:11:24 +0200

    Add codesigning on OS X

--- telldus-gui/cmake/FindSignTool.cmake
+++ telldus-gui/cmake/FindSignTool.cmake
@@ -1,10 +1,10 @@
 
-IF(WIN32)
+IF(WIN32 OR APPLE)
 	SET(SIGN_FILES FALSE CACHE BOOL "Sign files ofter build")
 ENDIF()
 
 FUNCTION(SIGN TARGET)
-	IF (NOT WIN32)
+	IF (NOT WIN32 AND NOT APPLE)
 		RETURN()
 	ENDIF()
 	IF (NOT SIGN_FILES)
@@ -12,8 +12,15 @@ FUNCTION(SIGN TARGET)
 	ENDIF()
 	GET_TARGET_PROPERTY(file ${TARGET} LOCATION)
 	GET_FILENAME_COMPONENT(filename ${file} NAME)
-	ADD_CUSTOM_COMMAND( TARGET ${TARGET} POST_BUILD
-		COMMAND signtool.exe sign /a /t http://timestamp.verisign.com/scripts/timstamp.dll ${file}
-		COMMENT "Signing file ${filename}"
-	)
+	IF (WIN32)
+		ADD_CUSTOM_COMMAND( TARGET ${TARGET} POST_BUILD
+			COMMAND signtool.exe sign /a /t http://timestamp.verisign.com/scripts/timstamp.dll ${file}
+			COMMENT "Signing file ${filename}"
+		)
+	ELSEIF(APPLE)
+		ADD_CUSTOM_COMMAND( TARGET ${TARGET} POST_BUILD
+			COMMAND codesign -v --force --sign "Developer ID Application: Telldus Technologies AB" ${file}
+			COMMENT "Signing file ${filename}"
+		)
+	ENDIF()
 ENDFUNCTION()
