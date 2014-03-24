2412d2abcbb639e5759321b910b9adec023fef01
Johan Ström <johan@stromnet.se>
2014-03-24 21:20:11 +0100

    FreeBSD compile fixes

--- telldus-core/common/Socket_unix.cpp
+++ telldus-core/common/Socket_unix.cpp
@@ -18,7 +18,7 @@
 #include "common/Strings.h"
 
 #define BUFSIZE 512
-#if defined(_MACOSX) && !defined(SOCK_CLOEXEC)
+#if (defined(_MACOSX) || defined (__FreeBSD__)) && !defined(SOCK_CLOEXEC)
 	#define SOCK_CLOEXEC 0
 #endif
 
